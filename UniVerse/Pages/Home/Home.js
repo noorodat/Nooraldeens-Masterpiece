19910d28672bd21a5272923e3c5556fed6aabfbb
B1UG <noor.feraas@gmail.com>
2023-07-29 23:21:20 +0300

    adde

---
+++ UniVerse/Pages/Home/Home.js
@@ -0,0 +1,13 @@
+// Start Menu
+
+let menu = document.getElementById("menu");
+
+menu.onclick = () => {
+    document.querySelector("nav .links").classList.toggle("active");
+}
+
+// End Menu
+
+
+
+
