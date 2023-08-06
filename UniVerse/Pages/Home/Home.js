44e8b50bfb95d8f6a21c9749e79b147a2071d1a1
noorodat <nooraldeen.aloudat@gmail.com>
2023-08-06 16:11:08 +0300

    added

--- UniVerse/Pages/Home/Home.js
+++ UniVerse/Pages/Home/Home.js
@@ -8,6 +8,20 @@ menu.onclick = () => {
 
 // End Menu
 
+// Start Text Opacity
+
+
+// Wait for the page to load
+window.addEventListener('load', function () {
+    // Get the .text element
+    const landingText = document.getElementById('landingText');
+    // Add the 'visible' class to make the element visible (opacity 1)
+    landingText.classList.add('visible');
+});
+
+// End Text Opacity
+
+
 
 
 
