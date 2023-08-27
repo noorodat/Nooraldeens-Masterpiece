8076528b2240e64bf7a33a8a4950fbd8337da3e3
B1UG <noor.feraas@gmail.com>
2023-08-27 22:18:09 +0300

    added from own pc

--- UniVerse/Pages/checkout/checkout.php
+++ UniVerse/Pages/checkout/checkout.php
@@ -9,21 +9,11 @@
     <meta name="author" content="Ansonika">
     <title>Checkout</title>
 
-    <!-- Favicons-->
-    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
-    <link rel="apple-touch-icon" type="image/x-icon" href="img/apple-touch-icon-57x57-precomposed.png">
-    <link rel="apple-touch-icon" type="image/x-icon" sizes="72x72" href="img/apple-touch-icon-72x72-precomposed.png">
-    <link rel="apple-touch-icon" type="image/x-icon" sizes="114x114" href="img/apple-touch-icon-114x114-precomposed.png">
-    <link rel="apple-touch-icon" type="image/x-icon" sizes="144x144" href="img/apple-touch-icon-144x144-precomposed.png">
-
-    <!-- GOOGLE WEB FONT -->
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
-
-    <!-- BASE CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
-	<link href="css/vendors.css" rel="stylesheet">
-	<link href="css/icon_fonts/css/all_icons.min.css" rel="stylesheet">
+    <!-- Start Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;600;700&display=swap" rel="stylesheet">
+    <!-- End Google Fonts -->
 
     <!-- YOUR CUSTOM CSS -->
     <link rel="stylesheet" href="./checkout.css">
@@ -42,7 +32,7 @@
 
     <?php include $_SERVER["DOCUMENT_ROOT"] . '/Masterpiece/Nooraldeens-Masterpiece/UniVerse/GlobalPHP/navbar.php'; ?>
 
-		<div class="bg_color_1">
+		<div class="">
 			<div class="container margin_60_35">
 				<div class="row">
 					<div class="col-lg-8">
--- UniVerse/Pages/courseDetails/courseDetails.php
+++ UniVerse/Pages/courseDetails/courseDetails.php
@@ -15,24 +15,10 @@
     <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;600;700&display=swap" rel="stylesheet">
     <!-- End Google Fonts -->
 
+
     <!-- Start CSS Links -->
     <?php include $_SERVER["DOCUMENT_ROOT"] . '/Masterpiece/Nooraldeens-Masterpiece/UniVerse/GlobalPHP/cssLinks.php'; ?>
 
-    <!-- Favicons-->
-    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
-    <link rel="apple-touch-icon" type="image/x-icon" href="img/apple-touch-icon-57x57-precomposed.png">
-    <link rel="apple-touch-icon" type="image/x-icon" sizes="72x72" href="img/apple-touch-icon-72x72-precomposed.png">
-    <link rel="apple-touch-icon" type="image/x-icon" sizes="114x114" href="img/apple-touch-icon-114x114-precomposed.png">
-    <link rel="apple-touch-icon" type="image/x-icon" sizes="144x144" href="img/apple-touch-icon-144x144-precomposed.png">
-
-    <!-- GOOGLE WEB FONT -->
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
-
-    <!-- BASE CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
-    <link href="css/vendors.css" rel="stylesheet">
-    <link href="css/icon_fonts/css/all_icons.min.css" rel="stylesheet">
 
     <!-- YOUR CUSTOM CSS -->
     <link href="./courseDetails.css" rel="stylesheet">
@@ -311,7 +297,7 @@ A machine learning course teaches the principles and techniques for creating alg
                         <aside class="col-lg-4" id="sidebar">
                             <div class="box_detail">
                                 <figure>
-                                    <a href="" class="video"><i class="arrow_triangle-right"></i><img src="../courses/images/machine learning.jpg" alt="machine learning" class="img-fluid"></a>
+                                    <a href="" class="video"><i class="fa-solid fa-play" id="playBtn"></i><img src="../courses/images/machine learning.jpg" alt="machine learning" class="img-fluid"></a>
                                 </figure>
                                 <div class="price-publisher p-2 d-flex align-items-center justify-content-between gap-2">
                                 <div class="price">
